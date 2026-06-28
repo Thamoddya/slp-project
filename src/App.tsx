27e83d442d7d8885eab5171e3dcfd05e012ecbc0
Thamoddya Rashmitha <thamoddyarashmithadissanayake@gmail.com>
2026-06-28 10:17:11 +0530

    Update public root route to display PosonWaiting page instead of PublicApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import AdminApp from "./pages/admin/AdminApp";
-import PublicApp from "./pages/PublicApp";
+import PosonWaiting from "./pages/PosonWaiting";
 
 export default function App() {
   return (
     <Routes>
       {/* Public root → Poson Poya "Stay Tuned" page */}
-      <Route path="/" element={<PublicApp />} />
+      <Route path="/" element={<PosonWaiting />} />
       {/* Admin panel remains fully accessible */}
       <Route path="/admin/*" element={<AdminApp />} />
       <Route path="*" element={<Navigate to="/" replace />} />
