3d4ee0cec095aeab54b5c47a574ac0bf3d592f58
Thamoddya Rashmitha <thamoddyarashmithadissanayake@gmail.com>
2026-06-29 12:17:57 +0530

    Remove PosonCurtain component from public root route, reverting to direct PublicApp rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import PosonCurtain from "./components/PosonCurtain";
 import AdminApp from "./pages/admin/AdminApp";
 import PublicApp from "./pages/PublicApp";
 
 export default function App() {
   return (
     <Routes>
-      {/* Public root → Poson Poya "Stay Tuned" page that opens like a curtain
-          into the real Pilgrim Route Guide. */}
-      <Route
-        path="/"
-        element={
-          <PosonCurtain>
-            <PublicApp />
-          </PosonCurtain>
-        }
-      />
-      {/* Admin panel remains fully accessible */}
+      <Route path="/" element={<PublicApp />} />
       <Route path="/admin/*" element={<AdminApp />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
